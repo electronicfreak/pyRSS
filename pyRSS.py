3a1b414d989e07570741bddf47a8bf6ee9a276aa
electronicfreak <github@electronicfreak.de>
2016-01-05 14:06:45 +0100

    2nd scrappy test

--- pyRSS.py
+++ pyRSS.py
@@ -3,11 +3,17 @@
 from lxml import html
 import requests
 
-def scrappy(url,key=""):
+def scrappy(url,key=False):
 	page = requests.get(url)
-	print(page.content)
 	tree = html.fromstring(page.content)
-	print("--------------------------------------------")
-	print(tree)
 	
-scrappy("http://192.168.1.1")
+	# '//div[@title="buyer-name"]/text()'
+	# '//span[@class="item-price"]/text()'
+	
+	r = []
+	
+	for k in key:
+		#print(k)
+		r.append(tree.xpath(k))
+	
+scrappy("http://rule34.xxx/index.php?page=post&s=list&tags=living_clothes",['//div//span//a/@href','//div//span//a//img/@alt'])
