21b7ab27857de9771372e50836224d80adf4dd27
electronicfreak <github@electronicfreak.de>
2016-01-09 01:54:12 +0100

    web debug

--- www/rss.php
+++ www/rss.php
@@ -1,19 +1,18 @@
 <?
+	header ("Content-Type:text/xml");
 	require('secret.php');
 	// startseite
 	$res = mysql_query("SELECT * FROM urls ORDER BY cat,ts");
 	$num = mysql_num_rows($res);
-	echo '<?xml version="1.0" encoding="UTF-8" ?>
+	echo '<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">';
+	echo '<channel><title></title><link>http://rss.electronicfreak.de</link><description></description>';
 	$cat = '';
 	for($i=0;$i<$num;$i++) {
 		$a = mysql_fetch_assoc($res);
 		if($cat != $a['cat']) {
-			if($cat != '') {
-				echo '</channel>';
-			}
-			echo '<channel><title>'. $a['cat'] .'</title><link>http://rss.electronicfreak.de</link><description></description>';
+			echo '</channel><channel><title>'. $a['cat'] .'</title><link>http://rss.electronicfreak.de</link><description></description>';
 		}
-		echo '<item><title>'. $a['ts'] .'</title><link>http://rss.electronicfreak.de/link.php?id='.$a['id'].'</link><description>'. nl2br($a['data']) .'</description></item>';
+		echo '<item><title>'. $a['url'] .'</title><link>http://rss.electronicfreak.de/link.php?id='.$a['id'].'</link><description>'. nl2br($a['data']) .'</description></item>';
 	}
 ?></channel></rss>
