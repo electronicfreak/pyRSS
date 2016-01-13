efc5409d7cd3f4695db20baf6421b1129c03a6d5
electronicfreak <github@electronicfreak.de>
2016-01-13 15:14:36 +0100

    untested

--- start.py
+++ start.py
@@ -2,30 +2,54 @@
 
 import config as c
 import secret as s
+import requsts
+
 
 # holt seite (str)
+#untestet
 def getPage(url):
-	
+	page = requests.get(url)
+	return page.content
 
 # seite beschneiden (str)
+#untestet
 def findStart(page,start,stop):
-	#TODO
+	start = page.find(start) + start.len()
+	stop = page.find(stop)
+	return page[start:stop]
 
 # zerteilt page in lines (list)
+#untestet
 def findLines(page,start,stop):
-	#TODO
+	loop = True
+	lines = []
+	while loop:
+		t1 = page.find(start) + start.len()
+		if t1 >= 0:
+			t2 = page.find(stop)
+			if t2 >= 0:
+				lines.append(page[t1:t2])
+				page = page[t2:]
+			else:
+				loop = False
+		else:
+			loop = False
+			
+		return lines
 
 # extrahiert daten (str)
-def findData(line,path,filter):
-	#TODO
+def findData(line,start,stop):
+	start = line.find(start) + start.len()
+	stop = line.find(stop)
+	return line[start:stop]
 
 for i in c.SEEDS:
 	page = getPage(i['url'])
 	page = findStart(page,i['start'],i['stop'])
 	lines = findLines(page,i['lStart'],['lStop'])
 	for j in lines:
-		url = findData(j,i['uPath'],i['uFilter'])
-		data = findData(j,i['dPath'],i['dFilter'])
-		title = findData(j,i['tPath'],i['tFilter'])
-		if sendData(url,title,data):
-			updateData(url,title,data)
+		url = findData(j,i['uStart'],i['uStop'])
+		data = findData(j,i['dStart'],i['dStop'])
+		title = findData(j,i['tStart'],i['tStop'])
+		#if sendData(url,title,data):
+		#	updateData(url,title,data)
